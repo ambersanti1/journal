711aff8f34ba357bb29d520ce9eafaccfa730977
ambersanti <carolinasandoval879@gmail.com>
2024-01-18 23:06:44 -0600

    deleted index.js

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const router = express.Router();
-const fs = require("fs");
-const PATH_ROUTES = __dirname;
+// const express = require("express");
+// const router = express.Router();
+// const fs = require("fs");
+// const PATH_ROUTES = __dirname;
 
-const removeExtension = (fileName) => {
-  return fileName.split('.').shift()
-}
+// const removeExtension = (fileName) => {
+//   return fileName.split('.').shift()
+// }
 
-const a = fs.readdirSync(PATH_ROUTES).filter((file) => {
-  const name = removeExtension(file)
-  if(name !== 'index') {
-    console.log(`Cargando ruta ${name} `);
-    router.use(`/${name}`, require(`./${file}`));
-  }
-})
+// const a = fs.readdirSync(PATH_ROUTES).filter((file) => {
+//   const name = removeExtension(file)
+//   if(name !== 'index') {
+//     console.log(`Cargando ruta ${name} `);
+//     router.use(`/${name}`, require(`./${file}`));
+//   }
+// })
 
 
-module.exports = router;
+// module.exports = router;
